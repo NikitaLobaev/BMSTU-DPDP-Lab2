b7bba704dce1fc2c5bc9b3b4552f06aabc90b969
Nikita Lobaev <nikitos2255@yandex.ru>
2020-11-21 12:36:54 +0300

    Scripted auto-commit on change (2020-11-21 12:36:54) by gitwatch.sh

--- src/main/java/lobaev/dpdp/lab2/AirportPartitioner.java
+++ src/main/java/lobaev/dpdp/lab2/AirportPartitioner.java
@@ -1,4 +1,12 @@
 package lobaev.dpdp.lab2;
 
-public class AirportPartitioner {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class AirportPartitioner extends Partitioner<AirportsWritable, Text> {
+
+    @Override
+    public int getPartition(AirportsWritable airportsWritable, Text text, int i) {
+        return 0;
+    }
 }
