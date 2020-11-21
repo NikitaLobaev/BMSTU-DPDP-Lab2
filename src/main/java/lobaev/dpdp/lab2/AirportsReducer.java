ed2f9c001e5d635705ab5f585c8ebb105f16d1bf
Nikita Lobaev <nikitos2255@yandex.ru>
2020-11-21 12:48:30 +0300

    Scripted auto-commit on change (2020-11-21 12:48:30) by gitwatch.sh

--- src/main/java/lobaev/dpdp/lab2/AirportsReducer.java
+++ src/main/java/lobaev/dpdp/lab2/AirportsReducer.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class AirportsReducer extends Reducer<AirportsWritable, Text, Text, Text> {
 
@@ -11,9 +12,24 @@ public class AirportsReducer extends Reducer<AirportsWritable, Text, Text, Text>
     protected void reduce(AirportsWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         //super.reduce(key, values, context);
         int c = 0;
-        String name; //название аэропорта
-        double delaysSum = 0, delayAverage, delayMin = Double.MIN_VALUE, delayMax = Double.MAX_VALUE;
-        
+        double delaysSum = 0, delayAverage, delayMin = Double.MAX_VALUE, delayMax = Double.MIN_VALUE;
+        Iterator<Text> iterator = values.iterator();
+        String name = iterator.next().toString(); //название аэропорта
+        while (iterator.hasNext()) {
+            double d = Double.parseDouble(iterator.next().toString());
+            delaysSum += d;
+            if (delayMin > d) {
+                delayMin = d;
+            }
+            if (delayMax < d) {
+                delayMax = d;
+            }
+            c++;
+        }
+        if (c > 0) {
+            delayAverage = delaysSum / c;
+            
+        }
     }
 
 }
