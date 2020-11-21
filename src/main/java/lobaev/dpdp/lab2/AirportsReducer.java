87259a44976babeecbee22008d3c8ff5883abd65
Nikita Lobaev <nikitos2255@yandex.ru>
2020-11-21 12:39:22 +0300

    Scripted auto-commit on change (2020-11-21 12:39:22) by gitwatch.sh

--- src/main/java/lobaev/dpdp/lab2/AirportsReducer.java
+++ src/main/java/lobaev/dpdp/lab2/AirportsReducer.java
@@ -1,2 +1,14 @@
-package lobaev.dpdp.lab2;public class AirportsReducer {
+package lobaev.dpdp.lab2;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class AirportsReducer extends Reducer<AirportsWritable, Text, Text, Text> {
+
+    @Override
+    protected void reduce(AirportsWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        super.reduce(key, values, context);
+    }
 }
