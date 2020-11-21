10a9ef8993dab5ea5b1e2e8154a352e4ea69f293
Nikita Lobaev <nikitos2255@yandex.ru>
2020-11-21 12:54:46 +0300

    Scripted auto-commit on change (2020-11-21 12:54:46) by gitwatch.sh

--- src/main/java/lobaev/dpdp/lab2/AirportsMapper.java
+++ src/main/java/lobaev/dpdp/lab2/AirportsMapper.java
@@ -1,4 +1,19 @@
 package lobaev.dpdp.lab2;
 
-public class AirportsMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class AirportsMapper extends Mapper<LongWritable, Text, AirportsWritable, Text> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        //super.map(key, value, context);
+        if (key.get() != 0) { //
+            String[] airports = value.toString().split(",");
+            context.write(new AirportsWritable(Integer.parseInt(airports[0]), 0), new Text(airports[1]));
+        }
+    }
 }
