bc7e6438a71e46cd88d99eaa89c582bb579bddd4
Nikita Lobaev <nikitos2255@yandex.ru>
2020-11-21 12:23:40 +0300

    Scripted auto-commit on change (2020-11-21 12:23:40) by gitwatch.sh

--- src/main/java/lobaev/dpdp/lab2/AirportsWritable.java
+++ src/main/java/lobaev/dpdp/lab2/AirportsWritable.java
@@ -2,11 +2,23 @@ package lobaev.dpdp.lab2;
 
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class AirportsWritable implements WritableComparable<AirportsWritable> {
 
     private int index, code;
 
     @Override
-    public void
+    public void write(DataOutput var1) throws IOException {
+        var1.writeInt(code);
+        var1.writeInt(index);
+    }
+
+    @Override
+    public void readFields(DataInput var1) throws IOException {
+
+    }
 
 }
