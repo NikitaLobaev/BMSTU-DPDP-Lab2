779f2abfa2f1a74c9a1f3279cd3b4a7b8a315116
Nikita Lobaev <nikitos2255@yandex.ru>
2020-11-21 12:39:53 +0300

    Scripted auto-commit on change (2020-11-21 12:39:53) by gitwatch.sh

--- src/main/java/lobaev/dpdp/lab2/AirportsReducer.java
+++ src/main/java/lobaev/dpdp/lab2/AirportsReducer.java
@@ -9,6 +9,8 @@ public class AirportsReducer extends Reducer<AirportsWritable, Text, Text, Text>
 
     @Override
     protected void reduce(AirportsWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        super.reduce(key, values, context);
+        //super.reduce(key, values, context);
+        
     }
+
 }
